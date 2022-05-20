7a399a4f1370c7ac75ef64b682cdd29767fb93a3
[Mae Sallean] <[mae@sallean.com]>
2022-05-19 22:31:39 -0500

    test push

--- HelloWorld Project/HelloWorld2.js
+++ HelloWorld Project/HelloWorld2.js
@@ -4,5 +4,6 @@ var fname = prompt("Enter your first name : ");
 var lname = prompt("Enter your last name : ");
 
 console.log("Wait");
+//test change
 
 console.log("Your Full Name is",fname,lname);

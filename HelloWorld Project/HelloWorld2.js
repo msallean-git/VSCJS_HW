90c6ddbe0475e9bb3c9dbe3041dd27c1c9bed1b9
[Mae Sallean] <[mae@sallean.com]>
2022-05-19 22:26:40 -0500

    Test Commit

--- HelloWorld Project/HelloWorld2.js
+++ HelloWorld Project/HelloWorld2.js
@@ -1,7 +1,8 @@
 const prompt = require('prompt-sync')();
 
 var fname = prompt("Enter your first name : ");
-var lname = prompt("Enter your last name :");
+var lname = prompt("Enter your last name : ");
 
+console.log("Wait");
 
 console.log("Your Full Name is",fname,lname);
